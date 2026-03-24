72a8c9a8dcf5b51c65374a66a9d4b695fb2c1119
Sabrina Akter Suchona <sabrinasuchona951@gmail.com>
2026-03-24 20:15:21 +0600

    Add files via upload

---
+++ JavaintroC251231.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaintro.c251231;
+
+public class JavaintroC251231 {
+
+   
+    public static void main(String[] args) {
+       String name = "John";
+    System.out.println("Hello " + name);
+    }
+    
+}
