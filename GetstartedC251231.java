8bf5e0d1274ad04a1c219c5ed0acbbffc0a0f769
Sabrina Akter Suchona <sabrinasuchona951@gmail.com>
2026-03-24 21:30:38 +0600

    Add files via upload

---
+++ GetstartedC251231.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package getstarted.c251231;
+
+
+public class GetstartedC251231 {
+
+   
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+    }
+    
+}
